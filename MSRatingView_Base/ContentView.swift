7ddb5cbcc315946735b514bc81dc791ed3f5885f
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-06-02 10:18:53 -0300

    Initial Commit

---
+++ MSRatingView_Base/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MSRatingView_Base
+//
+//  Created by Marcelo Sampaio on 02/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MSRatingView_Base/MSRatingView_BaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSRatingView_BaseApp.swift
+//  MSRatingView_Base
+//
+//  Created by Marcelo Sampaio on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MSRatingView_BaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
